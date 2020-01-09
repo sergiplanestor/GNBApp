7fed820fd03439c58634714535aaffcf307b3cf2
splanes <sergi.planestor@gmail.com>
2020-01-10 00:03:47 +0100

    RESTManager added

---
+++ app/src/main/java/bemobile/splanes/com/gnbapp/commons/RESTManager.kt
@@ -0,0 +1,30 @@
+package bemobile.splanes.com.gnbapp.commons
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class RESTManager {
+
+    companion object {
+        fun <T> createService(clazz: Class<T>): T {
+
+            val interceptor = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl("http://quiet-stone-2094.herokuapp.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build()
+                .create(clazz)
+        }
+    }
+}
