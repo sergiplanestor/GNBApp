dfabde6daeb2ad1c90048c4af4ba1d0308d62d7b
splanes <sergi.planestor@gmail.com>
2020-01-11 19:55:00 +0100

    Minor changes

--- app/src/main/java/bemobile/splanes/com/gnbapp/feature/ui/MainActivity.kt
+++ app/src/main/java/bemobile/splanes/com/gnbapp/feature/ui/MainActivity.kt
@@ -2,41 +2,34 @@ package bemobile.splanes.com.gnbapp.feature.ui
 
 import android.os.Bundle
 import android.view.animation.AnimationUtils
-import android.widget.Toast
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import bemobile.splanes.com.gnbapp.R
 import bemobile.splanes.com.gnbapp.commons.extension.app
 import bemobile.splanes.com.gnbapp.commons.ui.base.BaseActivity
 import bemobile.splanes.com.gnbapp.feature.dagger.TransactionModule
-import bemobile.splanes.com.gnbapp.feature.model.Transaction
-import bemobile.splanes.com.gnbapp.feature.service.GNBService
-import bemobile.splanes.com.gnbapp.feature.ui.adapter.OnTransactionClickListener
-import bemobile.splanes.com.gnbapp.feature.ui.adapter.TransactionAdapter
+import bemobile.splanes.com.gnbapp.feature.model.ProductItem
+import bemobile.splanes.com.gnbapp.feature.ui.adapter.OnProductClickListener
+import bemobile.splanes.com.gnbapp.feature.ui.adapter.ProductAdapter
 import bemobile.splanes.com.gnbapp.feature.viewmodel.TransactionViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import javax.inject.Inject
 
-class MainActivity : BaseActivity<TransactionViewModel>(), OnTransactionClickListener {
+class MainActivity : BaseActivity<TransactionViewModel>(), OnProductClickListener {
 
-    @Inject
-    lateinit var service: GNBService
-
-    lateinit var adapter: TransactionAdapter
+    lateinit var adapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
     }
 
     override fun initViews() {
         super.initViews()
 
-        collapsingToolbar.title = "Goliath National Bank"
+        setSupportActionBar(toolbar)
+        collapsingToolbar.title = getString(R.string.brand)
 
-        adapter = TransactionAdapter(context = this, transactions = listOf(), listener =  this)
-        transactionRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = ProductAdapter(context = this, products = listOf(), listener =  this)
+        transactionRecyclerView.layoutManager = GridLayoutManager(this, 2)
         transactionRecyclerView.layoutAnimation =
             AnimationUtils.loadLayoutAnimation(this, R.anim.layout_animation_fall_down)
         transactionRecyclerView.adapter = adapter
@@ -44,29 +37,32 @@ class MainActivity : BaseActivity<TransactionViewModel>(), OnTransactionClickLis
 
     override fun loadData() {
         super.loadData()
-        mViewModel.getTransactions().observe(this, Observer<List<Transaction>> { transactions ->
+        mViewModel.getProducts().observe(this, Observer<List<ProductItem>> { products ->
             runOnUiThread {
-                adapter.updateData(transactions = transactions)
+                adapter.updateData(products = products)
                 hideLoader()
             }
         })
     }
 
-    override fun onTransactionClick(transaction: Transaction) {
-        Toast.makeText(this, transaction.sku, Toast.LENGTH_SHORT).show()
+    override fun updateViews() {
+        super.updateViews()
+        loadData()
     }
 
-    override fun getViewModel(): Class<TransactionViewModel> {
+    override fun onProductClick(product: ProductItem) {
+        launchActivity(TransactionActivity::class.java, Bundle().apply {
+            putParcelable(TransactionActivity.EXTRA_PRODUCT, product)
+        })
+    }
+
+    override fun getLayoutResource(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun getViewModelClass(): Class<TransactionViewModel> {
         return TransactionViewModel::class.java
     }
-
-    /*override fun getViewModelFactory(): ViewModelProvider.Factory {
-        return object : ViewModelProvider.Factory {
-            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                return TransactionViewModel() as T
-            }
-        }
-    }*/
 
     override fun injectDaggerComponent() {
         app.component.plus(TransactionModule()).inject(this)
